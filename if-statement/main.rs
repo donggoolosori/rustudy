165db068f7badb226f548ddb4828d6e35da6115b
donggoolosori <kdj6551@naver.com>
2022-01-11 22:09:24 +0900

    if문

---
+++ if-statement/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let n = 20;
+
+    if n == 20 {
+        println!("20!");
+    } else if n > 20 {
+        println!("bigger than 20!");
+    } else {
+        println!("less than 20!");
+    }
+}
